a9780c697f50b2bf0c11a6260398a0ea1dbabe51
Mung2894 <vanmungxuanduong61@gmail.com>
2024-09-11 17:43:36 +0700

    45

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,6 +8,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with Hoi dan IT";
+        return "Hello World update";
+
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this";
+
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this";
+    }
+
 }
