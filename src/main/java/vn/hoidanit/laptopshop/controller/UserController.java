b84f50ab1ffd2656ecc6b5e557a4b9ebceb7fc3d
Mung2894 <vanmungxuanduong61@gmail.com>
2024-09-17 11:00:44 +0700

    60

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -6,20 +6,23 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import vn.hoidanit.laptopshop.domain.User;
+
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHelloWorld();
-        model.addAttribute("mung", test);
+
+        model.addAttribute("mung", "test");
         model.addAttribute("thu", "from controller with model");
         return "hello";
     }
@@ -35,6 +38,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User mung) {
         System.out.println("run here " + mung);
+        this.userService.handleSaveUser(mung);
         return "hello";
     }
 }
