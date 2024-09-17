a29e3a3b4408cb7cdfb3c1c7ab4c7750a75365ac
Mung2894 <vanmungxuanduong61@gmail.com>
2024-09-17 18:04:35 +0700

    66

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,6 +37,12 @@ public class UserController {
         model.addAttribute("users1", users);
         // System.out.println("User" + users);
         return "admin/user/table-user";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        return "admin/user/show";
     }
 
     @RequestMapping("/admin/user/create") // GET
